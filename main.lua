35429f4338e26ea9b52919c780dd263bbbc1794a
Rhiannon Michelmore <rhiannon.michelmore@gmail.com>
2013-12-17 22:08:24 +0000

    Updated so it shows a better ascii map, added some screenshots

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 function love.load()
   --allows you to change size of map
-  mapheight=100
-  mapwidth=60
+  mapheight=45
+  mapwidth=100
 
   --creates global "map" (eventually will be the equivalent of 2D array)
   map = {}
@@ -20,8 +20,8 @@ function love.load()
   end
 
   --just prints an ascii representation of finished map to terminal
-  for a=1,100 do
-    for b=1,100 do
+  for a=1,mapheight do
+    for b=1,mapwidth do
       if map[a][b]==1 then
         io.write("X")
       else
